bcda34a5736dda37cec4d3f5fcefe1a5401bb4ad
yuji nunome <yuji.dis@gmail.com>
2019-03-14 21:55:00 +0900

    tuika

---
+++ config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./configure --with-features=huge --enable-multibyte --enable-gpm --enable-cscope --enable-fontset --enable-fail-if-missing --enable-pythoninterp=dynamic --enable-python3interp=dynamic --enable-rubyinterp=dynamic  --prefix=/data/data/com.termux/files/usr
+
+
